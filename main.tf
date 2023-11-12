88bf3363c8da9cce5dbd33cca815b305a2c4959a
defenceindepth <adetayoaina09@gmail.com>
2023-11-12 16:01:03 -0500

    Update main.tf with corrected IAM configuration

--- main.tf
+++ main.tf
@@ -26,12 +26,17 @@ resource "aws_iam_group_membership" "desola_membership" {
   group = aws_iam_group.production_group.name
 }
 
-resource "aws_iam_user_policy_attachment" "lolu_admin_attachment" {
-  user       = aws_iam_user.lolu_user.name
+resource "aws_iam_group_policy_attachment" "production_group_admin_attachment" {
+  group      = aws_iam_group.production_group.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-resource "aws_iam_user_policy_attachment" "desola_admin_attachment" {
-  user       = aws_iam_user.desola_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+terraform {
+  backend "s3" {
+    bucket         = "desolalolu-terraform-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_locks"
+  }
 }
